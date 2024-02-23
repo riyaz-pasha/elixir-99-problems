2706c2b5efc8543d002a46424331de0faea97ade
Riyaz <riyazpasha880@gmail.com>
2024-02-24 03:37:28 +0530

    [Riyaz] Add. 22. List within given range

--- problem21/sol21.ex
+++ problem21/sol21.ex
@@ -2,7 +2,7 @@ defmodule Problem21 do
   def insert_at(new_element,input,position), do: insert_at_internal(new_element,input,position,1)
 
   defp insert_at_internal(_,[],_,_), do: []
-  
+
   defp insert_at_internal(new_element,[head|tail],position,current_position) when position===current_position do
     [new_element,head|tail]
   end
---
+++ problem22/sol22.ex
@@ -0,0 +1,15 @@
+defmodule Problem22 do
+  def range(start, stop), do: range_internal(start,stop)
+
+  defp range_internal(start, stop) when start===stop do
+    [stop]
+  end
+
+  defp range_internal(start, stop) do
+    [start | range_internal(start+1,stop)]
+  end
+end
+
+IO.inspect(Problem22.range(1,3))
+IO.inspect(Problem22.range(1,9))
+IO.inspect(Problem22.range(5,15))
