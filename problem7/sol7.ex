105a2a798487f1f6ace87635ce4dca43bf388ba8
Riyaz <riyazpasha880@gmail.com>
2024-02-23 09:09:19 +0530

    [Riyaz] Add. Flatten list v2

--- problem7/sol7.ex
+++ problem7/sol7.ex
@@ -1,23 +1,17 @@
 defmodule Problem7 do
-  def flatten([]), do: []
 
   def flatten(input) do
     flatten_internal(input)
   end
 
-  def flatten_internal([head|tail]) do
+  defp flatten_internal([]), do: []
+
+  defp flatten_internal([head|tail]) do
     head_output= cond do
-      head===[] -> []
       is_list(head) -> flatten_internal(head)
       true -> [head]
     end
-
-    tail_output= cond do
-      tail===[] -> []
-      is_list(tail) -> flatten_internal(tail)
-      true -> [tail]
-    end
-
+    tail_output= flatten_internal(tail)
     head_output ++ tail_output
   end
 end
@@ -26,6 +20,8 @@ IO.inspect(Problem7.flatten([]))
 IO.inspect(Problem7.flatten([1]))
 IO.inspect(Problem7.flatten([[1]]))
 IO.inspect(Problem7.flatten([[1,2]]))
+IO.inspect(Problem7.flatten([[[11,22]]]))
 IO.inspect(Problem7.flatten([[1],2]))
 IO.inspect(Problem7.flatten([1,[2]]))
 IO.inspect(Problem7.flatten([[1,[3,[4]]],[2]]))
+IO.inspect(Problem7.flatten([111,[222,[333,[444],555],666]]))
