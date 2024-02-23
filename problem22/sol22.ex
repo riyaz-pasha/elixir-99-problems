39e18e56ba13e4e20052c9ea98f5b9598968b540
Riyaz <riyazpasha880@gmail.com>
2024-02-24 03:40:27 +0530

    [Riyaz] Add. 22. handle scenario when start > stop

--- problem22/sol22.ex
+++ problem22/sol22.ex
@@ -1,6 +1,7 @@
 defmodule Problem22 do
   def range(start, stop), do: range_internal(start,stop)
 
+  defp range_internal(start, stop) when start>stop, do: [start | range_internal(start-1,stop)]
   defp range_internal(start, stop) when start===stop do
     [stop]
   end
@@ -13,3 +14,4 @@ end
 IO.inspect(Problem22.range(1,3))
 IO.inspect(Problem22.range(1,9))
 IO.inspect(Problem22.range(5,15))
+IO.inspect(Problem22.range(15,1))
