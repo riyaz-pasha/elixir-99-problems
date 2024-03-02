e62673cd02ba9398800e20825fe5607a9a570575
Riyaz <riyazpasha880@gmail.com>
2024-03-02 13:05:20 +0530

    [Riyaz] Add. 34. Prime factors of 25

--- problem34/problem34.ex
+++ problem34/problem34.ex
@@ -4,6 +4,7 @@ defmodule Problem34 do
   def primeFactors(num) when num<=1, do: []
   def primeFactors(num) when rem(num,2)===0, do: [2 | primeFactors(trunc(num/2))]
   def primeFactors(num) when rem(num,3)===0, do: [3 | primeFactors(trunc(num/3))]
+  def primeFactors(num) when rem(num,5)===0, do: [5 | primeFactors(trunc(num/5))]
   def primeFactors(num), do: [num]
 end
 
@@ -42,5 +43,9 @@ defmodule Problem34Tests do
     assert Problem34.primeFactors(12)===[2,2,3]
     assert Problem34.primeFactors(13)===[13]
     assert Problem34.primeFactors(14)===[2,7]
+  end
+
+  test "factors of 25" do
+    assert Problem34.primeFactors(25)===[5,5]
   end
 end
