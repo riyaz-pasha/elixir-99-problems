28df2b649722b08feba2ef1ce05279dd2d933939
Riyaz <riyazpasha880@gmail.com>
2024-03-02 12:57:25 +0530

    [Riyaz] Add. 34. Prime factors of 4

--- problem34/problem34.ex
+++ problem34/problem34.ex
@@ -2,6 +2,7 @@ ExUnit.start()
 
 defmodule Problem34 do
   def primeFactors(num) when num<=1, do: []
+  def primeFactors(num) when rem(num,2)===0, do: [2 | primeFactors(trunc(num/2))]
   def primeFactors(num), do: [num]
 end
 
@@ -17,5 +18,9 @@ defmodule Problem34Tests do
 
   test "factors of 3" do
     assert Problem34.primeFactors(3)===[3]
+  end
+
+  test "factors of 4" do
+    assert Problem34.primeFactors(4)===[2,2]
   end
 end
